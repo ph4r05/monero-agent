ab2de3b9aebb75c29910213b9100e716e0c14d12
Dusan Klinec <dusan.klinec@gmail.com>
2018-03-11 18:30:13 +0100

    monero related misc utils moved to monero.py

---
+++ monero_glue/monero.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from mnero import mininero
+from monero_serialize import xmrtypes, xmrserialize
+from . import common as common
+
+
+class TsxData(xmrserialize.MessageType):
+    """
+    TsxData, initial input to the transaction processing.
+    Serialization structure for easy hashing.
+    """
+    FIELDS = [
+        ('payment_id', xmrserialize.BlobType),
+        ('outputs', xmrserialize.ContainerType, xmrtypes.TxDestinationEntry),
+        ('change_dts', xmrtypes.TxDestinationEntry),
+    ]
+
+    def __init__(self, payment_id=None, outputs=None, change_dts=None, **kwargs):
+        super().__init__(**kwargs)
+
+        self.payment_id = payment_id
+        self.change_dts = change_dts
+        self.outputs = outputs if outputs else []  # type: list[xmrtypes.TxDestinationEntry]
+
+
+def addr_to_hash(addr: xmrtypes.AccountPublicAddress):
+    """
+    Creates hashable address representation
+    :param addr:
+    :return:
+    """
+    return bytes(addr.m_spend_public_key + addr.m_view_public_key)
+
+
+def classify_subaddresses(tx_dests, change_addr : xmrtypes.AccountPublicAddress):
+    """
+    Classify destination subaddresses
+    void classify_addresses()
+    :param tx_dests:
+    :type tx_dests: list[xmrtypes.TxDestinationEntry]
+    :param change_addr:
+    :return:
+    """
+    num_stdaddresses = 0
+    num_subaddresses = 0
+    single_dest_subaddress = None
+    addr_set = set()
+    for tx in tx_dests:
+        if change_addr and change_addr == tx.addr:
+            continue
+        addr_hashed = addr_to_hash(tx.addr)
+        if addr_hashed in addr_set:
+            continue
+        addr_set.add(addr_hashed)
+        if tx.is_subaddress:
+            num_subaddresses+=1
+            single_dest_subaddress = tx.addr
+        else:
+            num_stdaddresses+=1
+    return num_stdaddresses, num_subaddresses, single_dest_subaddress
+
+
+async def parse_extra_fields(extra_buff):
+    """
+    Parses extra buffer to the extra fields vector
+    :param extra_buff:
+    :return:
+    """
+    extras = []
+    rw = xmrserialize.MemoryReaderWriter(extra_buff)
+    ar2 = xmrserialize.Archive(rw, False)
+    while len(rw.buffer) > 0:
+        extras.append(await ar2.variant(elem_type=xmrtypes.TxExtraField))
+    return extras
+
+
+async def find_tx_extra_field_by_type(extra_fields, msg):
+    """
+    Finds given message type in the extra array, or returns None if not found
+    :param extra_fields:
+    :param msg:
+    :return:
+    """
+    for x in extra_fields:
+        if isinstance(x, msg):
+            return x
+
--- monero_glue/trezor.py
+++ monero_glue/trezor.py
@@ -2,76 +2,20 @@
 # -*- coding: utf-8 -*-
 from mnero import mininero
 from monero_serialize import xmrtypes, xmrserialize
+from .monero import TsxData, classify_subaddresses, addr_to_hash
+from . import monero
 from . import common as common
-
-
-class TsxData(xmrserialize.MessageType):
-    """
-    TsxData, initial input to the transaction processing.
-    Serialization structure for easy hashing.
-    """
-    FIELDS = [
-        ('payment_id', xmrserialize.BlobType),
-        ('outputs', xmrserialize.ContainerType, xmrtypes.TxDestinationEntry),
-        ('change_dts', xmrtypes.TxDestinationEntry),
-    ]
-
-    def __init__(self, payment_id=None, outputs=None, change_dts=None, **kwargs):
-        super().__init__(**kwargs)
-
-        self.payment_id = payment_id
-        self.change_dts = change_dts
-        self.outputs = outputs if outputs else []  # type: list[xmrtypes.TxDestinationEntry]
-
-
-def addr_to_hash(addr: xmrtypes.AccountPublicAddress):
-    """
-    Creates hashable address representation
-    :param addr:
-    :return:
-    """
-    return bytes(addr.m_spend_public_key + addr.m_view_public_key)
-
-
-def classify_subaddresses(tx_dests, change_addr : xmrtypes.AccountPublicAddress):
-    """
-    Classify destination subaddresses
-    void classify_addresses()
-    :param tx_dests:
-    :type tx_dests: list[xmrtypes.TxDestinationEntry]
-    :param change_addr:
-    :return:
-    """
-    num_stdaddresses = 0
-    num_subaddresses = 0
-    single_dest_subaddress = None
-    addr_set = set()
-    for tx in tx_dests:
-        if change_addr and change_addr == tx.addr:
-            continue
-        addr_hashed = addr_to_hash(tx.addr)
-        if addr_hashed in addr_set:
-            continue
-        addr_set.add(addr_hashed)
-        if tx.is_subaddress:
-            num_subaddresses+=1
-            single_dest_subaddress = tx.addr
-        else:
-            num_stdaddresses+=1
-    return num_stdaddresses, num_subaddresses, single_dest_subaddress
 
 
 class Trezor(object):
     def __init__(self):
         self.tsx_ctr = 0
-        self.tsx_data = None
         self.tsx_obj = None
         self.key_master = None
         self.key_hmac = None
 
     async def init_transaction(self, tsx_data: TsxData):
         self.tsx_ctr += 1
-        self.tsx_data = tsx_data
         self.tsx_obj = TTransaction()
         self.tsx_obj.init_transaction(tsx_data)
 
@@ -91,7 +35,6 @@ class Trezor(object):
         :return:
         """
         self.tsx_obj.set_input(src_entr)
-
 
 
 class TTransaction(object):
