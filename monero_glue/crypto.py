472ea359d2e6dd2830bfbe9ff4ea3b62f2c60df2
Dusan Klinec <dusan.klinec@gmail.com>
2018-03-12 16:17:52 +0100

    crypto - point representations, extended, H_p() comment

--- monero_glue/crypto.py
+++ monero_glue/crypto.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import math
+import sys
+import operator
+
 from Crypto.Random import random as rand
 from mnero import mnemonic  # making 25 word mnemonic to remember your keys
 import binascii  # conversion between hex, int, and binary. Also for the crc32 thing
@@ -11,6 +14,30 @@ from mnero.mininero import b, q, l
 from mnero import mininero, keccak2
 from monero_serialize import xmrtypes, xmrserialize
 from . import common as common
+
+
+# Useful for very coarse version differentiation.
+PY3 = sys.version_info[0] == 3
+
+if PY3:
+    indexbytes = operator.getitem
+    intlist2bytes = bytes
+    int2byte = operator.methodcaller('to_bytes', 1, 'big')
+
+else:
+    int2byte = chr
+    range = xrange
+
+    def indexbytes(buf, i):
+        return ord(buf[i])
+
+    def intlist2bytes(l):
+        return b"".join(chr(c) for c in l)
+
+
+# Extended curve coordinates
+B_ext = (ed25519.Bx % q, ed25519.By % q, 1, (ed25519.Bx * ed25519.By) % q)
+I_ext = (0, 1, 1, 0)
 
 
 def b16_to_scalar(bts):
@@ -56,6 +83,112 @@ def encodepoint(P):
     :return:
     """
     return ed25519.encodepoint(P)
+
+
+def pow2(x, p):
+    """== pow(x, 2**p, q)"""
+    while p > 0:
+        x = x * x % q
+        p -= 1
+    return x
+
+
+def inv(z):
+    """$= z^{-1} \mod q$, for z != 0"""
+    # Adapted from curve25519_athlon.c in djb's Curve25519.
+    z2 = z * z % q                                # 2
+    z9 = pow2(z2, 2) * z % q                      # 9
+    z11 = z9 * z2 % q                             # 11
+    z2_5_0 = (z11 * z11) % q * z9 % q             # 31 == 2^5 - 2^0
+    z2_10_0 = pow2(z2_5_0, 5) * z2_5_0 % q        # 2^10 - 2^0
+    z2_20_0 = pow2(z2_10_0, 10) * z2_10_0 % q     # ...
+    z2_40_0 = pow2(z2_20_0, 20) * z2_20_0 % q
+    z2_50_0 = pow2(z2_40_0, 10) * z2_10_0 % q
+    z2_100_0 = pow2(z2_50_0, 50) * z2_50_0 % q
+    z2_200_0 = pow2(z2_100_0, 100) * z2_100_0 % q
+    z2_250_0 = pow2(z2_200_0, 50) * z2_50_0 % q   # 2^250 - 2^0
+    return pow2(z2_250_0, 5) * z11 % q            # 2^255 - 2^5 + 11 = q - 2
+
+
+def encodepoint_ext(P):
+    (x, y, z, t) = P
+    zi = inv(z)
+    x = (x * zi) % q
+    y = (y * zi) % q
+    bits = [(y >> i) & 1 for i in range(b - 1)] + [x & 1]
+
+    # noinspection PyTypeChecker
+    return b''.join([
+        int2byte(sum([bits[i * 8 + j] << j for j in range(8)]))
+        for i in range(b // 8)
+    ])
+
+
+def isoncurve_ext(P):
+    (x, y, z, t) = P
+    return (z % q != 0 and
+            x*y % q == z*t % q and
+            (y*y - x*x - z*z - ed25519.d*t*t) % q == 0)
+
+
+def decodepoint_ext(s):
+    x,y = decodepoint(s)
+    P = (x, y, 1, (x*y) % q)
+    if not isoncurve_ext(P):
+        raise ValueError("decoding point that is not on curve")
+    return P
+
+
+def conv_xy_to_ext(P):
+    """
+    Converts x,y representation to extended form
+    (x % q, y % q, 1, x*y % q)
+    :param P:
+    :return:
+    """
+    return P[0] % q, P[1] % q, 1, (P[0] * P[1]) % q
+
+
+def conv_ext_to_xy(P):
+    """
+    Converts extended representation to x,y
+    :param P:
+    :return:
+    """
+    (x, y, z, t) = P
+    zi = inv(z)
+    x = (x * zi) % q
+    y = (y * zi) % q
+    return x, y
+
+
+def conv_xy_to_precomp(P):
+    """
+    Transform x,y representation to precomputed form
+    :param P:
+    :return:
+    """
+    x,y = P
+    return y-x, y+x, 2*ed25519.d*x*y
+
+
+def conv_ext_to_proj(P):
+    """
+    Extended coordinates to projective
+    :param P:
+    :return:
+    """
+    return P[0], P[1], P[2]
+
+
+def conv_p1p1_to_ext(P):
+    """
+    p1p1 representation to extended
+    :param P:
+    :return:
+    """
+    x0, y0, z0, t0 = P
+    return x0*t0 % q, y0*z0 % q, z0*t0 % q, x0*y0 % q
 
 
 def public_key(sk):
@@ -267,6 +400,9 @@ def derive_secret_key(derivation, output_index, base):
 def hash_to_ec(key):
     """
     H_p(data)
+
+    https://archive.is/o/yfINb/https://github.com/ShenNoether/ge_fromfe_writeup/blob/master/ge_fromfe.pdf
+    http://archive.is/yfINb
     :param key:
     :return:
     """
