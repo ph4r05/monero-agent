3b8e466006160d233166e4e1836a84f09462eadc
Dusan Klinec <dusan.klinec@gmail.com>
2018-03-12 14:17:32 +0100

    test - creds init fix

--- monero_glue_test/test_basic.py
+++ monero_glue_test/test_basic.py
@@ -101,8 +101,8 @@ class Basetest(aiounittest.AsyncTestCase):
 
         trez = trezor.Trezor()
         trez.creds = trezor.WalletCreds.new_wallet(
-            priv_view_key=int(b'4ce88c168e0f5f8d6524f712d5f8d7d83233b1e7a2a60b5aba5206cc0ea2bc08', 16),
-            priv_spend_key=int(b'f2644a3dd97d43e87887e74d1691d52baa0614206ad1b0c239ff4aa3b501750a', 16))
+            priv_view_key=crypto.b16_to_scalar(b'4ce88c168e0f5f8d6524f712d5f8d7d83233b1e7a2a60b5aba5206cc0ea2bc08'),
+            priv_spend_key=crypto.b16_to_scalar(b'f2644a3dd97d43e87887e74d1691d52baa0614206ad1b0c239ff4aa3b501750a'))
 
         print(js)
 
