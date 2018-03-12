cf241d0cbaf97e26a2037913ea59a3c636e10cce
Dusan Klinec <dusan.klinec@gmail.com>
2018-03-12 17:42:26 +0100

    ed25519ietf refactoring

--- mnero/ed25519ietf.py
+++ mnero/ed25519ietf.py
@@ -27,24 +27,26 @@
 #either expressed or implied, of the FreeBSD Project.
 
 import hashlib
+from . import mininero
 
 def sha512(s):
     return hashlib.sha512(s).digest()
 
+
 # Base field Z_p
 p = 2**255 - 19
+
 
 def modp_inv(x):
     return pow(x, p-2, p)
 
+
 # Curve constant
 d = -121665 * modp_inv(121666) % p
 
+
 # Group order
 q = 2**252 + 27742317777372353535851937790883648493
-
-def sha512_modq(s):
-    return int.from_bytes(sha512(s), "little") % q
 
 # Points are represented as tuples (X, Y, Z, T) of extended coordinates,
 # with x = X/Z, y = Y/Z, x*y = T/Z
@@ -60,6 +62,7 @@ def point_add(P, Q):
     H = B+A
     return (E*F, G*H, F*G, E*H)
 
+
 # Computes Q = s * Q
 def point_mul(s, P):
     Q = (0, 1, 1, 0)  # Neutral element
@@ -70,7 +73,8 @@ def point_mul(s, P):
         P = point_add(P, P)
         s >>= 1
     return Q
-    
+
+
 def point_equal(P, Q):
     # x1 / z1 == x2 / z2  <==>  x1 * z2 == x2 * z1
     if (P[0] * Q[2] - Q[0] * P[2]) % p != 0:
@@ -78,9 +82,11 @@ def point_equal(P, Q):
     if (P[1] * Q[2] - Q[1] * P[2]) % p != 0:
         return False
     return True
-    
-    # Square root of -1
+
+
+# Square root of -1
 modp_sqrt_m1 = pow(2, (p-1) // 4, p)
+
 
 # Compute corresponding x coordinate, with low bit corresponding to sign,
 # or return None on failure
@@ -103,25 +109,29 @@ def recover_x(y, sign):
         x = p - x
     return x
 
+
 # Base point
 g_y = 4 * modp_inv(5) % p
 g_x = recover_x(g_y, 0)
 G = (g_x, g_y, 1, g_x * g_y % p)
 
+
 def basepoint():
     return G
+
 
 def point_compress(P):
     zinv = modp_inv(P[2])
     x = P[0] * zinv % p
     y = P[1] * zinv % p
-    return MiniNero.intToHex(y | ((x & 1) << 255)  )
+    return mininero.intToHex(y | ((x & 1) << 255))
+
 
 def point_decompress(s):
     #if len(s) != 32:
         #raise Exception("Invalid input length for decompression")
     #y = int.from_bytes(s, "little")
-    y = MiniNero.hexToInt(s)
+    y = mininero.hexToInt(s)
     sign = y >> 255
     y &= (1 << 255) - 1
 
@@ -129,4 +139,5 @@ def point_decompress(s):
     if x is None:
         return None
     else:
-        return (x, y, 1, x*y % p)
+        return x, y, 1, x*y % p
+
