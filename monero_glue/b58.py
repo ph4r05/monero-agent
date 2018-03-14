f6a016b9cd68b12772561e09612239954be7cff4
Dusan Klinec <dusan.klinec@gmail.com>
2018-03-15 00:40:46 +0100

    b58 replaced by working implementation

--- monero_glue/b58.py
+++ monero_glue/b58.py
@@ -1,51 +1,128 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Author: Dusan Klinec, ph4r05, 2018
+# https://github.com/keis/base58/blob/master/base58.py
 
 
-import binascii
+'''Base58 encoding
+Implementations of Base58 and Base58Check endcodings that are compatible
+with the bitcoin network.
+'''
 
-__b58chars = b'123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-__b58base = len(__b58chars)
+# This module is based upon base58 snippets found scattered over many bitcoin
+# tools written in python. From what I gather the original source is from a
+# forum post by Gavin Andresen, so direct your praise to him.
+# This module adds shiny packaging and support for python3.
+
+from hashlib import sha256
+
+__version__ = '0.2.5'
+
+# 58 character alphabet used
+alphabet = b'123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
 
-def bit(h, i):
-    return ((h[i // 8]) >> (i % 8)) & 1
+if bytes == str:  # python2
+    iseq, bseq, buffer = (
+        lambda s: map(ord, s),
+        lambda s: ''.join(map(chr, s)),
+        lambda s: s,
+    )
+else:  # python3
+    iseq, bseq, buffer = (
+        lambda s: s,
+        bytes,
+        lambda s: s.buffer,
+    )
 
 
-def hex_to_int(h):
-    s = binascii.unhexlify(h)
-    bb = len(h) * 4
-    return sum(2 ** i * bit(s, i) for i in range(0, bb))
+def scrub_input(v):
+    if isinstance(v, str) and not isinstance(v, bytes):
+        v = v.encode('ascii')
+
+    if not isinstance(v, bytes):
+        raise TypeError(
+            "a bytes-like object is required (also str), not '%s'" %
+            type(v).__name__)
+
+    return v
 
 
-def rev_bytes(a):
-    b = [a[i:i + 2] for i in range(0, len(a) - 1, 2)]
-    return b''.join(b[::-1])
+def b58encode_int(i, default_one=True):
+    '''Encode an integer using Base58'''
+    if not i and default_one:
+        return alphabet[0:1]
+    string = b""
+    while i:
+        i, idx = divmod(i, 58)
+        string = alphabet[idx:idx+1] + string
+    return string
 
 
 def b58encode(v):
-    a = [rev_bytes(v[i:i + 16]) for i in range(0, len(v) - 16, 16)]
-    rr = -2 * ((len(v) // 2) % 16)
+    '''Encode a string using Base58'''
 
-    res = b''
-    for b in a:
-        bb = hex_to_int(b)
-        result = b''
-        while bb >= __b58base:
-            div, mod = divmod(bb, __b58base)
-            result = __b58chars[mod:mod + 1] + result
-            bb = div
-        result = __b58chars[bb:bb + 1] + result
-        res += result
-    result = b''
-    if rr < 0:
-        bf = hex_to_int(rev_bytes(v[rr:]))  # since we only reversed the ones in the array..
-        result = b''
-        while bf >= __b58base:
-            div, mod = divmod(bf, __b58base)
-            result = __b58chars[mod:mod + 1] + result
-            bf = div
-        result = __b58chars[bf:bf + 1] + result
-    res += result
-    return res
+    v = scrub_input(v)
+
+    nPad = len(v)
+    v = v.lstrip(b'\0')
+    nPad -= len(v)
+
+    p, acc = 1, 0
+    for c in iseq(reversed(v)):
+        acc += p * c
+        p = p << 8
+
+    result = b58encode_int(acc, default_one=False)
+
+    return (alphabet[0:1] * nPad + result)
+
+
+def b58decode_int(v):
+    '''Decode a Base58 encoded string as an integer'''
+
+    v = scrub_input(v)
+
+    decimal = 0
+    for char in v:
+        decimal = decimal * 58 + alphabet.index(char)
+    return decimal
+
+
+def b58decode(v):
+    '''Decode a Base58 encoded string'''
+
+    v = scrub_input(v)
+
+    origlen = len(v)
+    v = v.lstrip(alphabet[0:1])
+    newlen = len(v)
+
+    acc = b58decode_int(v)
+
+    result = []
+    while acc > 0:
+        acc, mod = divmod(acc, 256)
+        result.append(mod)
+
+    return (b'\0' * (origlen - newlen) + bseq(reversed(result)))
+
+
+def b58encode_check(v):
+    '''Encode a string using Base58 with a 4 character checksum'''
+
+    digest = sha256(sha256(v).digest()).digest()
+    return b58encode(v + digest[:4])
+
+
+def b58decode_check(v):
+    '''Decode and verify the checksum of a Base58 encoded string'''
+
+    result = b58decode(v)
+    result, check = result[:-4], result[-4:]
+    digest = sha256(sha256(result).digest()).digest()
+
+    if check != digest[:4]:
+        raise ValueError("Invalid checksum")
+
+    return result
+
---
+++ monero_glue_test/test_b58.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Dusan Klinec, ph4r05, 2018
+
+import random
+import base64
+import unittest
+import pkg_resources
+import requests
+import asyncio
+import aiounittest
+import binascii
+
+
+from monero_glue import b58
+
+
+class Base58Test(aiounittest.AsyncTestCase):
+    """Simple tests"""
+
+    def __init__(self, *args, **kwargs):
+        super(Base58Test, self).__init__(*args, **kwargs)
+
+    def test_base(self):
+        """
+        Simple b58 encode test
+        :return:
+        """
+        self.assertIsNotNone(b58.b58encode(b''))
+        self.assertIsNotNone(b58.b58encode(binascii.unhexlify(b'1234567890')))
+        self.assertIsNotNone(b58.b58encode(binascii.unhexlify(b'1234567890'*50)))
+        self.assertEqual(b58.b58encode(binascii.unhexlify(b'0092A1A1E820F70A881E529C844178B56FE745DCBE4588659E')),
+                         b'1ENKHRkBKtXnSHuQt1LR2VFdPtw6NvbrfX')
+
+
+if __name__ == "__main__":
+    unittest.main()  # pragma: no cover
+
