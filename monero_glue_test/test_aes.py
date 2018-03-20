c71071611f74f38e6ebcfe3d187f187e1e2fd2a4
Dusan Klinec <dusan.klinec@gmail.com>
2018-03-20 21:30:13 +0100

    test aes added

---
+++ monero_glue_test/test_aes.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Dusan Klinec, ph4r05, 2018
+
+import random
+import base64
+import unittest
+import pkg_resources
+import requests
+import asyncio
+import aiounittest
+import binascii
+
+from monero_glue import aesgcm, common
+
+
+class AesTest(aiounittest.AsyncTestCase):
+    """Simple tests"""
+
+    def __init__(self, *args, **kwargs):
+        super(AesTest, self).__init__(*args, **kwargs)
+
+    def test_enc(self):
+        key = common.keccak_hash(b'0')
+        plain = b'1234'
+        assoc = b'4567'
+        iv, cip, tag = aesgcm.encrypt(key, plain, assoc)
+
+        plain2 = aesgcm.decrypt(key, iv, cip, tag, assoc)
+        self.assertEqual(plain, plain2)
+
+        with self.assertRaises(ValueError):
+            plain2 = aesgcm.decrypt(key, iv, cip, tag, assoc + b'1')
+
+        with self.assertRaises(ValueError):
+            plain2 = aesgcm.decrypt(key, bytes(iv[0] ^ 0xff) + iv[1:], cip, tag, assoc)
+
+        with self.assertRaises(ValueError):
+            plain2 = aesgcm.decrypt(key, iv, bytes(cip[0] ^ 0xff) + cip[1:], tag, assoc)
+
+
+if __name__ == "__main__":
+    unittest.main()  # pragma: no cover
+
+
