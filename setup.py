b4d45d398978dc9d6cbb49c41cab5b247b7d0387
Dusan Klinec <dusan.klinec@gmail.com>
2018-04-21 23:53:20 +0200

    monero-serialize updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ version = '0.1.2'
 
 # Please update tox.ini when modifying dependency version requirements
 install_requires = [
-    'monero-serialize>=0.1.0',
+    'monero-serialize>=1.1.0',
     'pycryptodome',
 ]
 
