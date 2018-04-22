fad379353fe9bf03812747fd46e668a5ad7786ac
Dusan Klinec <dusan.klinec@gmail.com>
2018-04-22 12:34:24 +0200

    chacha20 auth encryption implemented

---
+++ monero_glue/chacha.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Dusan Klinec, ph4r05, 2018
+
+from Crypto.Cipher import ChaCha20
+import pycryptonight
+from . import crypto
+
+
+def generate_key(data):
+    """
+    Chacha key derivation used in Monero
+    :param data:
+    :return:
+    """
+    return pycryptonight.cn_slow_hash(data)
+
+
+def encrypt_xmr(priv_key, plaintext, authenticated=True):
+    """
+    Monero-like authenticated encryption with Chacha20 and EC signature
+
+    :param priv_key:
+    :param plaintext:
+    :param authenticated:
+    :return:
+    """
+    key = generate_key(crypto.encodeint(priv_key))
+    ciphertext = encrypt(key, plaintext)
+    if not authenticated:
+        return ciphertext
+
+    hash = crypto.cn_fast_hash(ciphertext)
+    c, r, pub = crypto.generate_signature(hash, priv_key)
+    signature = crypto.encodeint(c) + crypto.encodeint(r)
+    return ciphertext + signature
+
+
+def decrypt_xmr(priv_key, ciphertext, authenticated=True):
+    """
+    Monero-like authenticated decryption with Chacha20 and EC signature
+
+    :param priv_key:
+    :param ciphertext:
+    :param authenticated:
+    :return:
+    """
+    if authenticated:
+        ciphertext, signature = ciphertext[:-64], ciphertext[-64:]
+        hash = crypto.cn_fast_hash(ciphertext)
+        pub = crypto.scalarmult_base(priv_key)
+        c = crypto.decodeint(signature[:32])
+        r = crypto.decodeint(signature[32:])
+        res = crypto.check_signature(hash, c, r, pub)
+        if not res:
+            raise ValueError('Signature invalid')
+
+    key = generate_key(crypto.encodeint(priv_key))
+    return decrypt(key, ciphertext)
+
+
+def encrypt(key, plaintext):
+    """
+    Uses Chacha20 for encryption
+    :param key:
+    :param plaintext:
+    :return: nonce+ciphertext
+    """
+    cipher = ChaCha20.new(key=key)
+    ciphertext = cipher.encrypt(plaintext)
+
+    nonce = cipher.nonce
+    res = nonce + ciphertext
+    return res
+
+
+def decrypt(key, ciphertext):
+    """
+    Chacha20 decryption
+    :param key:
+    :param ciphertext:
+    :return:
+    """
+    nonce, ciptext = ciphertext[:8], ciphertext[8:]
+    cipher = ChaCha20.new(key=key, nonce=nonce)
+    return cipher.decrypt(ciptext)
+
+
---
+++ monero_glue_test/test_chacha.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Dusan Klinec, ph4r05, 2018
+
+import unittest
+
+import aiounittest
+
+from monero_glue import crypto
+from monero_glue import chacha
+
+
+class ChachaTest(aiounittest.AsyncTestCase):
+    """Simple tests"""
+
+    def __init__(self, *args, **kwargs):
+        super(ChachaTest, self).__init__(*args, **kwargs)
+
+    def test_encrypt_base(self):
+        for i in range(10):
+            key = crypto.cn_fast_hash(crypto.encodeint(crypto.random_scalar()))
+            data = crypto.cn_fast_hash(crypto.encodeint(crypto.random_scalar())) * (i+1)
+
+            ciphertext = chacha.encrypt(key, data)
+            plaintext = chacha.decrypt(key, ciphertext)
+            self.assertEqual(plaintext, data)
+
+            plaintext2 = chacha.decrypt(key, bytes(int(ciphertext[0]) ^ 0xff) + ciphertext[1:])
+            self.assertNotEqual(plaintext2, data)
+
+    def test_encrypt(self):
+        self.back_encrypt(False)
+
+    def test_encrypt_auth(self):
+        self.back_encrypt(True)
+
+    def back_encrypt(self, authenticated=True):
+        for i in range(5):
+            priv_key = crypto.random_scalar()
+            data = crypto.cn_fast_hash(crypto.encodeint(crypto.random_scalar())) * (i + 1)
+
+            blob = chacha.encrypt_xmr(priv_key, data, authenticated=authenticated)
+            plaintext = chacha.decrypt_xmr(priv_key, blob, authenticated=authenticated)
+            self.assertEqual(data, plaintext)
+
+            try:
+                plaintext2 = chacha.decrypt_xmr(priv_key + 1, blob, authenticated=authenticated)
+                if authenticated:
+                    self.fail('Signature error expected')
+                else:
+                    self.assertNotEqual(data, plaintext2)
+
+            except:
+                if not authenticated:
+                    raise
+
+
+if __name__ == "__main__":
+    unittest.main()  # pragma: no cover
+
+
