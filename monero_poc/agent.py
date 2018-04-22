b95fcfe1eae30aca06ffc43ef4a34509d129ad7d
Dusan Klinec <dusan.klinec@gmail.com>
2018-04-22 19:57:16 +0200

    poc: agent async

--- monero_poc/agent.py
+++ monero_poc/agent.py
@@ -2,13 +2,18 @@
 # -*- coding: utf-8 -*-
 # Author: Dusan Klinec, ph4r05, 2018
 
+import os
+import asyncio
 import argparse
 import binascii
 import logging
+import json
+import requests
 import coloredlogs
 
-from monero_glue import crypto, monero
-
+from monero_glue import crypto, monero, agent_lite, trezor_lite, wallet
+from monero_glue.monero import TsxData
+from monero_serialize import xmrboost, xmrtypes, xmrserialize, xmrobj, xmrjson
 
 logger = logging.getLogger(__name__)
 coloredlogs.CHROOT_FILES = []
@@ -23,26 +28,38 @@ class HostAgent(object):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.args = None
-        self.network_type = None
 
-    def entry(self):
+        self.network_type = None
+        self.address = None
+        self.priv_view = None
+        self.pub_view = None
+        self.pub_spend = None
+
+        self.loop = asyncio.get_event_loop()
+
+    async def entry(self):
         """
         pass
         :return:
         """
         priv_view = self.args.view_key.encode('utf8')
-        priv_view_key = crypto.b16_to_scalar(priv_view)
-        pub_view_comp = crypto.scalarmult_base(priv_view_key)
+        self.priv_view = crypto.b16_to_scalar(priv_view)
+        self.address = self.args.address.encode('utf8')
+        self.pub_view = crypto.scalarmult_base(self.priv_view)
 
-        version, pub_spend, pub_view = monero.decode_addr(self.args.address.encode('utf8'))
-        if not crypto.point_eq(pub_view_comp, crypto.decodepoint(pub_view)):
+        version, pub_spend, pub_view = monero.decode_addr(self.address)
+        self.pub_spend = crypto.decodepoint(pub_spend)
+
+        if not crypto.point_eq(self.pub_view, crypto.decodepoint(pub_view)):
             raise ValueError('Computed view public key does not match the one from address')
 
         print('Ver: %s' % version)
         print('Public spend key: %s' % binascii.hexlify(pub_spend).decode('utf8'))
         print('Public view key : %s' % binascii.hexlify(pub_view).decode('utf8'))
 
-    def main(self):
+        logger.info('Terminating')
+
+    async def main(self):
         """
         Entry point
         :return:
@@ -56,15 +73,18 @@ class HostAgent(object):
                             help='Hex coded private view key')
 
         self.args = parser.parse_args()
-        self.entry()
+        await self.entry()
 
 
-def main():
+async def main():
     agent = HostAgent()
-    agent.main()
+    await agent.main()
 
 
 if __name__ == '__main__':
-    main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    # loop.run_forever()
+    loop.close()
 
 
