40ccc5afa6d11b290054d46544e7d9bb10c6fd31
Dusan Klinec <dusan.klinec@gmail.com>
2018-04-23 10:43:27 +0200

    agent: misc added, exceptions

---
+++ monero_glue/agent_misc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Dusan Klinec, ph4r05, 2018
+
+
+class AgentError(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class TrezorReturnedError(AgentError):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
