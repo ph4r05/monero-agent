cac813c6d34b4e0b77fca498c498ec7118200524
Dusan Klinec <dusan.klinec@gmail.com>
2018-05-05 20:28:48 +0200

    poc/agent: transformed to cmd iface

--- monero_poc/agent.py
+++ monero_poc/agent.py
@@ -14,9 +14,16 @@ import argparse
 import binascii
 import logging
 import requests
+import functools
 import coloredlogs
 import pickle
 import sys
+import time
+import json
+import concurrent
+import threading
+from blessed import Terminal
+from cmd2 import Cmd
 
 from . import trace_logger
 from monero_glue import agent_lite, agent_misc, trezor_lite
@@ -40,6 +47,12 @@ class TrezorProxy(trezor_lite.TrezorLite):
 
     async def ping(self):
         resp = requests.get('%s/ping' % self.endpoint)
+        resp.raise_for_status()
+        return resp.json()
+
+    async def watch_only(self):
+        resp = requests.get('%s/watch_only' % self.endpoint)
+        resp.raise_for_status()
         return resp.json()
 
     async def transfer(self, method, cmd, payload):
@@ -90,10 +103,11 @@ class TrezorProxy(trezor_lite.TrezorLite):
         return await self.transfer_pickle('tx_sign', 'final', *args, **kwargs)
 
 
-class HostAgent(object):
+class HostAgent(Cmd):
     """
     Host agent wrapper
     """
+    prompt = '$> '
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -107,46 +121,235 @@ class HostAgent(object):
 
         self.trace_logger = trace_logger.Tracelogger(logger)
         self.loop = asyncio.get_event_loop()
+        self.t = Terminal()
+        self.worker_loop = asyncio.new_event_loop()
+        self.worker_thread = threading.Thread(target=self.looper, args=(self.worker_loop, ))
+        self.worker_thread.setDaemon(True)
+        self.worker_thread.start()
+
         self.trezor_proxy = TrezorProxy()
         self.agent = agent_lite.Agent(self.trezor_proxy)
 
-    async def entry(self):
+    def looper(self, loop):
         """
-        pass
+        Main looper
+        :param loop:
         :return:
         """
-        if self.args.debug:
-            coloredlogs.install(level=logging.DEBUG, use_chroot=False)
+        asyncio.set_event_loop(loop)
+        loop.run_forever()
 
+    def submit_coro(self, coro):
+        """
+        Submits corroutine to the worker loop
+        :param fnc:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        return asyncio.run_coroutine_threadsafe(coro, self.worker_loop)
+
+    def wait_coro(self, coro):
+        """
+        Runs corouting, waits for result
+        :param fnc:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        future = self.submit_coro(coro)
+        return future.result()
+
+    #
+    # CLI
+    #
+
+    def update_intro(self):
+        """
+        Updates intro text for CLI header - adds version to it.
+        :return:
+        """
+        self.intro = '-'*self.get_term_width() + \
+                     '\n    Monero Trezor agent\n' + \
+                     '\n' + \
+                     '-' * self.get_term_width()
+
+    def get_term_width(self):
+        """
+        Returns terminal width
+        :return: terminal width in characters or 80 if exception encountered
+        """
+        try:
+            width = self.t.width
+            if width is None or width <= 0:
+                return 80
+
+            return width
+        except:
+            pass
+        return 80
+
+    #
+    # Handlers
+    #
+
+    do_q = quit
+    do_Q = quit
+
+    def do_ping(self, line):
+        try:
+            pres = self.wait_coro(self.trezor_proxy.ping())
+            print('OK %s' % pres)
+
+        except Exception as e:
+            print('Trezor not connected')
+            logger.debug(e)
+
+    def do_sign(self, line):
+        self.wait_coro(self.sign_wrap(line))
+
+    complete_sign = Cmd.path_complete
+
+    #
+    # Logic
+    #
+
+    async def is_connected(self):
+        """
+        Returns True if Trezor is connected
+        :return:
+        """
+        try:
+            await self.trezor_proxy.ping()
+            return True
+
+        except Exception as e:
+            return False
+
+    async def load_watchonly(self):
+        """
+        Loads watch-only credentials from connected Trezor
+        :return:
+        """
+        if not await self.is_connected():
+            logger.error('Trezor is not connected')
+            raise agent_misc.TrezorNotRunning('Could not load watch-only credentials')
+
+        try:
+            print('Loading watch-only credentials from Trezor. Please, confirm the request on Trezor.')
+            res = await self.trezor_proxy.watch_only()
+
+            self.priv_view = crypto.b16_to_scalar(res['data']['view_key'].encode('utf8'))
+            self.address = res['data']['address'].encode('utf8')
+            await self.open_with_keys(self.priv_view, self.address)
+
+        except Exception as e:
+            raise ValueError(e)
+
+    async def open_account(self):
+        """
+        Opens the watch only account
+        :return:
+        """
+        creds_passed = self.args.view_key is not None and self.args.address is not None
+        account_file_set = self.args.account_file is not None
+        account_file_ex = os.path.exists(self.args.account_file) if account_file_set else False
+        if creds_passed:
+            await self.open_account_passed()
+        elif account_file_ex:
+            await self.open_account_file(self.args.account_file)
+        else:
+            await self.load_watchonly()
+
+        # Write acquired data to the account file
+        if account_file_set and not account_file_ex:
+            with open(self.args.account_file, 'w') as fh:
+                data = {
+                    'view_key': binascii.hexlify(crypto.encodeint(self.priv_view)).decode('utf8'),
+                    'address': self.address.decode('utf8'),
+                }
+                json.dump(data, fh)
+
+        print('Public spend key: %s' % binascii.hexlify(crypto.encodepoint(self.pub_spend)).decode('utf8'))
+        print('Public view key : %s' % binascii.hexlify(crypto.encodepoint(self.pub_view)).decode('utf8'))
+        print('Address:          %s' % self.address.decode('utf8'))
+
+    async def open_account_passed(self):
+        """
+        Loads passed credentials
+        :return:
+        """
         priv_view = self.args.view_key.encode('utf8')
         self.priv_view = crypto.b16_to_scalar(priv_view)
         self.address = self.args.address.encode('utf8')
-        self.pub_view = crypto.scalarmult_base(self.priv_view)
+        await self.open_with_keys(self.priv_view, self.address)
 
-        version, pub_spend, pub_view = monero.decode_addr(self.address)
+    async def open_account_file(self, file):
+        """
+        Opens account file
+        :param file:
+        :return:
+        """
+        with open(file) as fh:
+            js = json.load(fh)
+
+        self.priv_view = crypto.b16_to_scalar(js['view_key'].encode('utf8'))
+        self.address = js['address'].encode('utf8')
+        await self.open_with_keys(self.priv_view, self.address)
+
+    async def open_with_keys(self, view_key, address):
+        """
+        Processess view key private + address
+        :param view_key:
+        :param address:
+        :return:
+        """
+        self.pub_view = crypto.scalarmult_base(view_key)
+
+        version, pub_spend, pub_view = monero.decode_addr(address)
         self.pub_spend = crypto.decodepoint(pub_spend)
 
         if not crypto.point_eq(self.pub_view, crypto.decodepoint(pub_view)):
             raise ValueError('Computed view public key does not match the one from address')
 
-        print('Ver: %s' % version)
-        print('Public spend key: %s' % binascii.hexlify(pub_spend).decode('utf8'))
-        print('Public view key : %s' % binascii.hexlify(pub_view).decode('utf8'))
+    async def entry(self):
+        """
+        Entry point
+        :return:
+        """
+        if self.args.debug:
+            coloredlogs.install(level=logging.DEBUG, use_chroot=False)
+
+        await self.open_account()
 
         if self.args.sign:
-            try:
-                return await self.sign(self.args.sign)
+            return await self.sign_wrap(self.args.sign)
 
-            except agent_misc.TrezorReturnedError as e:
-                self.trace_logger.log(e)
-                print('Trezor returned an error: %s' % e)
-                return 1
-
-            except agent_misc.TrezorNotRunning as e:
-                logger.error('Trezor server is not running')
-                return 2
-
+        self.update_intro()
+        self.cmdloop()
         logger.info('Terminating')
+
+    async def sign_wrap(self, file):
+        """
+        Sign wrapper
+        :param file:
+        :return:
+        """
+        if not self.priv_view:
+            logger.error('View key not set, cannot sign')
+            return -3
+
+        try:
+            return await self.sign(file)
+
+        except agent_misc.TrezorReturnedError as e:
+            self.trace_logger.log(e)
+            print('Trezor returned an error: %s' % e)
+            return 1
+
+        except agent_misc.TrezorNotRunning as e:
+            logger.error('Trezor server is not running')
+            return 2
 
     async def sign(self, file):
         """
@@ -237,11 +440,14 @@ class HostAgent(object):
         """
         parser = argparse.ArgumentParser(description='Trezor CLI based host client')
 
-        parser.add_argument('--address', dest='address', required=True,
+        parser.add_argument('--address', dest='address',
                             help='Full address')
 
-        parser.add_argument('--view-key', dest='view_key', required=True,
+        parser.add_argument('--view-key', dest='view_key',
                             help='Hex coded private view key')
+
+        parser.add_argument('--account-file', dest='account_file',
+                            help='Account file with watch-only creds')
 
         parser.add_argument('--rpc-addr', dest='rpc_addr', default='127.0.0.1:18081',
                             help='RPC address for tsx relay')
@@ -255,8 +461,13 @@ class HostAgent(object):
         parser.add_argument('--debug', dest='debug', default=False, action='store_const', const=True,
                             help='Debugging output')
 
-        self.args = parser.parse_args()
-        return await self.entry()
+        args_src = sys.argv
+        self.args, unknown = parser.parse_known_args(args=args_src[1:])
+
+        sys.argv = [args_src[0]]
+        res = await self.entry()
+        sys.argv = args_src
+        return res
 
 
 async def main():
