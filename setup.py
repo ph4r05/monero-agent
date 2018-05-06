0db45fab7bbce65f8bca40491adc04e7d5dd1f5f
Dusan Klinec <dusan.klinec@gmail.com>
2018-05-06 18:04:14 +0200

    dep: serialize version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ version = '1.0.0'
 
 # Please update tox.ini when modifying dependency version requirements
 install_requires = [
-    'monero-serialize>=1.1.3',
+    'monero-serialize>=1.1.5',
     'pycryptodome',
     'py-cryptonight>=0.1.2',
 ]
