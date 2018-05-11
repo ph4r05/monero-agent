3ed50dd3d75afea5a5e874bd74bcb1bfe13e1296
Dusan Klinec <dusan.klinec@gmail.com>
2018-05-11 18:40:15 +0200

    xmr/tcry: scalar values ops

--- monero_glue/xmr/core/backend/trezor_crypto.py
+++ monero_glue/xmr/core/backend/trezor_crypto.py
@@ -9,11 +9,74 @@ CLIB = ct.cdll.LoadLibrary(os.path.join(os.path.dirname(__file__), './libtrezor-
 # Functions
 CLIB.random_init.restype = ct.c_int
 
-CLIB.ge25519_pack.argtypes = [tt.KEY_BUFF, ct.POINTER(tt.Ge25519)]
-CLIB.ge25519_unpack_vartime.argtypes = [ct.POINTER(tt.Ge25519), tt.KEY_BUFF]
+
+#
+# MODM / scalar values
+#
+
 
 CLIB.contract256_modm.argtypes = [tt.KEY_BUFF, tt.MODM]
 CLIB.expand256_modm.argtypes = [tt.MODM, ct.c_void_p, ct.c_size_t]
+CLIB.add256_modm.argtypes = [tt.MODM, tt.MODM, tt.MODM]
+CLIB.neg256_modm.argtypes = [tt.MODM, tt.MODM]
+CLIB.sub256_modm.argtypes = [tt.MODM, tt.MODM, tt.MODM]
+CLIB.mul256_modm.argtypes = [tt.MODM, tt.MODM, tt.MODM]
+CLIB.reduce256_modm.argtypes = [tt.MODM]
+CLIB.barrett_reduce256_modm.argtypes = [tt.MODM, tt.MODM, tt.MODM]
+CLIB.set256_modm.argtypes = [tt.MODM, ct.c_uint64]
+
+CLIB.eq256_modm.argtypes = [tt.MODM, tt.MODM]
+CLIB.eq256_modm.restype = ct.c_int
+
+CLIB.cmp256_modm.argtypes = [tt.MODM, tt.MODM]
+CLIB.cmp256_modm.restype = ct.c_int
+
+CLIB.iszero256_modm.argtypes = [tt.MODM]
+CLIB.iszero256_modm.restype = ct.c_int
+
+CLIB.copy256_modm.argtypes = [tt.MODM, tt.MODM]
+
+CLIB.check256_modm.argtypes = [tt.MODM]
+CLIB.check256_modm.restype = ct.c_int
+
+CLIB.mulsub256_modm.argtypes = [tt.MODM, tt.MODM, tt.MODM, tt.MODM]
+
+
+#
+# FE
+#
+
+
+CLIB.curve25519_copy.argtypes = [tt.FE, tt.FE]
+CLIB.curve25519_add.argtypes = [tt.FE, tt.FE, tt.FE]
+CLIB.curve25519_add_after_basic.argtypes = [tt.FE, tt.FE, tt.FE]
+CLIB.curve25519_add_reduce.argtypes = [tt.FE, tt.FE, tt.FE]
+CLIB.curve25519_sub.argtypes = [tt.FE, tt.FE, tt.FE]
+CLIB.curve25519_scalar_product.argtypes = [tt.FE, tt.FE, ct.c_uint32]
+CLIB.curve25519_sub_after_basic.argtypes = [tt.FE, tt.FE, tt.FE]
+CLIB.curve25519_sub_reduce.argtypes = [tt.FE, tt.FE, tt.FE]
+CLIB.curve25519_neg.argtypes = [tt.FE, tt.FE]
+CLIB.curve25519_mul.argtypes = [tt.FE, tt.FE, tt.FE]
+CLIB.curve25519_square.argtypes = [tt.FE, tt.FE]
+CLIB.curve25519_square_times.argtypes = [tt.FE, tt.FE, ct.c_int]
+CLIB.curve25519_expand.argtypes = [tt.FE, tt.KEY_BUFF]
+CLIB.curve25519_contract.argtypes = [tt.KEY_BUFF, tt.FE]
+CLIB.curve25519_recip.argtypes = [tt.FE, tt.FE]
+
+#
+# GE
+#
+
+
+CLIB.ge25519_pack.argtypes = [tt.KEY_BUFF, ct.POINTER(tt.Ge25519)]
+CLIB.ge25519_unpack_vartime.argtypes = [ct.POINTER(tt.Ge25519), tt.KEY_BUFF]
+
+
+
+#
+# XMR
+#
+
 
 CLIB.xmr_hash_to_ec.argtypes = [ct.c_void_p, ct.c_size_t, ct.POINTER(tt.Ge25519)]
 
@@ -63,6 +126,83 @@ def contract256_modm(sc):
     buff = tt.KEY_BUFF()
     CLIB.contract256_modm(buff, sc)
     return bytes(bytearray(buff))
+
+
+def add256_modm(a, b):
+    r = tt.MODM()
+    CLIB.add256_modm(r, a, b)
+    return r
+
+
+def neg256_modm(a):
+    r = tt.MODM()
+    CLIB.neg256_modm(r, a)
+    return r
+
+
+def sub256_modm(a, b):
+    r = tt.MODM()
+    CLIB.sub256_modm(r, a, b)
+    return r
+
+
+def mul256_modm(a, b):
+    r = tt.MODM()
+    CLIB.mul256_modm(r, a, b)
+    return r
+
+
+def reduce256_modm(a):
+    CLIB.reduce256_modm(a)
+    return a
+
+
+def barrett_reduce256_modm(a, b):
+    r = tt.MODM()
+    CLIB.barrett_reduce256_modm(r, a, b)
+    return r
+
+
+def set256_modm(a, b):
+    CLIB.set256_modm(a, ct.c_uint64(b))
+
+
+def init256_modm(a):
+    r = tt.MODM()
+    CLIB.set256_modm(r, ct.c_uint64(a))
+    return r
+
+
+def eq256_modm(a, b):
+    return CLIB.eq256_modm(a, b)
+
+
+def cmp256_modm(a, b):
+    return CLIB.cmp256_modm(a, b)
+
+
+def iszero256_modm(a, b):
+    return CLIB.iszero256_modm(a, b)
+
+
+def copy256_modm(a, b):
+    return CLIB.copy256_modm(a, b)
+
+
+def clone256_modm(a):
+    r = tt.MODM()
+    CLIB.copy256_modm(r, a)
+    return r
+
+
+def check256_modm(a):
+    return CLIB.copy256_modm(a)
+
+
+def mulsub256_modm(a, b, c):
+    r = tt.MODM()
+    CLIB.mulsub256_modm(r, a, b, c)
+    return r
 
 
 def xmr_hash_to_ec(buff):
