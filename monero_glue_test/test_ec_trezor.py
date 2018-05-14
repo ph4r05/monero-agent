aaf462fa1ef71ce6c3ece389d9afa2065a5bfde4
Dusan Klinec <dusan.klinec@gmail.com>
2018-05-14 18:07:10 +0200

    tests/ec_trezor: fixed if no trezor bin is present

--- monero_glue_test/test_ec_trezor.py
+++ monero_glue_test/test_ec_trezor.py
@@ -4,14 +4,25 @@
 
 import unittest
 import binascii
+import logging
 
 import aiounittest
 
 from monero_glue.xmr import crypto, common
-from monero_glue.xmr.core import ec_trezor
-from monero_glue.xmr.core.backend import trezor_crypto as tcry
 from monero_glue.xmr.core import ec_py
-from monero_glue.xmr.core import ec_trezor
+
+
+logger = logging.getLogger(__name__)
+
+
+try:
+    from monero_glue.xmr.core import ec_trezor
+    from monero_glue.xmr.core.backend import trezor_crypto as tcry
+    LOADED = 1
+
+except Exception as e:
+    logger.info('Trezor backend loading error: %s' % e)
+    LOADED = 0
 
 
 class EcTrezorTest(aiounittest.AsyncTestCase):
@@ -21,17 +32,24 @@ class EcTrezorTest(aiounittest.AsyncTestCase):
         super(EcTrezorTest, self).__init__(*args, **kwargs)
 
     def test_ed_crypto(self):
-        h = binascii.unhexlify(b'8b655970153799af2aeadc9ff1add0ea6c7251d54154cfa92c173a0dd39c1f94')
+        if not LOADED:
+            self.skipTest('Trezor crypto missing')
+            
+        h_hex = b'8b655970153799af2aeadc9ff1add0ea6c7251d54154cfa92c173a0dd39c1f94'
+        h = binascii.unhexlify(h_hex)
         pt = tcry.ge25519_unpack_vartime_r(h)
-        print(pt)
-        print(binascii.hexlify(tcry.ge25519_pack_r(pt)))
+        packed = tcry.ge25519_pack_r(pt)
+        self.assertEqual(h, packed)
 
     def test_modm(self):
+        if not LOADED:
+            self.skipTest('Trezor crypto missing')
+
         x = ec_py.encodeint(0xaa)
         y = ec_trezor.decodeint(x)
-        print(y)
         yy = ec_trezor.encodeint(y)
-        print(binascii.hexlify(yy))
+        self.assertEqual(yy[0], 0xaa)
+        self.assertEqual(yy[1], 0x00)
 
 
 if __name__ == "__main__":
