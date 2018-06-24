64185cd2806e29d3a813c40b8448f42940de5d82
Dusan Klinec <dusan.klinec@gmail.com>
2018-06-24 18:00:43 +0200

    trezor_misc adapted from trezor-core

--- monero_glue/trezor_misc.py
+++ monero_glue/trezor_misc.py
@@ -3,6 +3,9 @@
 # Author: Dusan Klinec, ph4r05, 2018
 
 from monero_glue.xmr import common, crypto
+from monero_glue.xmr.monero import TsxData
+from monero_glue.messages import MoneroTxDestinationEntry, MoneroTsxData
+from monero_serialize import xmrtypes, xmrserialize
 
 
 class TrezorError(Exception):
@@ -50,6 +53,52 @@ def compute_tx_key(spend_key_private, tx_prefix_hash, salt=None, rand_mult=None)
     tx_key = crypto.pbkdf2(passwd, salt, count=100)
     return tx_key, salt, rand_mult
 
+
+async def translate_monero_dest_entry(dst_entry: MoneroTxDestinationEntry):
+    d = xmrtypes.TxDestinationEntry()
+    d.amount = dst_entry.amount
+    d.is_subaddress = dst_entry.is_subaddress
+    d.addr = xmrtypes.AccountPublicAddress(m_spend_public_key=dst_entry.addr.m_spend_public_key,
+                                           m_view_public_key=dst_entry.addr.m_view_public_key)
+    return d
+
+
+async def translate_tsx_data(tsx_data: MoneroTsxData):
+    tsxd = TsxData()
+    for fld in TsxData.MFIELDS:
+        fname = fld[0]
+        if hasattr(tsx_data, fname):
+            setattr(tsxd, fname, getattr(tsx_data, fname))
+
+    if tsx_data.change_dts:
+        tsxd.change_dts = translate_monero_dest_entry(tsx_data.change_dts)
+
+    tsxd.outputs = [translate_monero_dest_entry(x) for x in tsx_data.outputs]
+    return tsxd
+
+
+async def parse_msg(bts, msg):
+    reader = xmrserialize.MemoryReaderWriter(bts)
+    ar = xmrserialize.Archive(reader, False)
+    return await ar.message(msg)
+
+
+async def parse_src_entry(bts):
+    return await parse_msg(bts, xmrtypes.TxSourceEntry())
+
+
+async def parse_dst_entry(bts):
+    return await parse_msg(bts, xmrtypes.TxDestinationEntry())
+
+
+async def parse_vini(bts):
+    return await parse_msg(bts, xmrtypes.TxinToKey())
+
+
+async def dump_msg(msg):
+    writer = xmrserialize.MemoryReaderWriter()
+    ar = xmrserialize.Archive(writer, True)
+    return await ar.message(msg)
 
 
 
