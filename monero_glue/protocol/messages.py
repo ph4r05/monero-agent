e56694e9b74d2c55c35680c6f3dc364152ac31b3
Dusan Klinec <dusan.klinec@gmail.com>
2018-06-28 00:04:39 +0200

    protocol: message converter added

--- monero_glue/protocol/messages.py
+++ monero_glue/protocol/messages.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # Author: Dusan Klinec, ph4r05, 2018
 
-
+from monero_glue import protobuf
 from monero_glue import messages
 from monero_glue.messages import *
 
@@ -24,4 +24,56 @@ def get_message_from_type(type_id):
         if cur_id == type_id:
             return getattr(messages, m)
     raise ValueError('Could not find message with type: %s' % type_id)
+
+
+class MessageConverter(object):
+    """
+    Converts protobuf messages between base packages.
+    Converts our protobufs <-> trezorlib
+    Required for protobuf encoders
+    """
+    
+    def __init__(self):
+        self.imported = False
+        self.tlib_msgs = None
+        self.tlib_p = None
+
+    def _init(self):
+        if self.imported:
+            return
+
+        from trezorlib import messages as tlib_msgs
+        from trezorlib import protobuf as tlib_protobuf
+        self.tlib_msgs = tlib_msgs
+        self.tlib_p = tlib_protobuf
+        self.imported = True
+
+    def _transform(self, msg, src_proto, dst_package):
+        if isinstance(msg, src_proto.MessageType):
+            msg_name = msg.__class__.__name__
+            tlib_msg = getattr(dst_package, msg_name)
+
+            nmsg = tlib_msg()
+            for tag in msg.FIELDS:
+                field = msg.FIELDS[tag]
+                field_name = field[0]
+                if hasattr(msg, field_name):
+                    cval = getattr(msg, field_name, None)
+                    setattr(nmsg, field_name, self.to_trezorlib(cval))
+            return nmsg
+
+        elif isinstance(msg, list):
+            return [self.to_trezorlib(x) for x in msg]
+        elif isinstance(msg, dict):
+            return {v:self.to_trezorlib(x) for v,x in msg.items()}
+        else:
+            return msg
+
+    def to_phlib(self, msg):
+        self._init()
+        return self._transform(msg, self.tlib_p, messages)
+
+    def to_trezorlib(self, msg):
+        self._init()
+        return self._transform(msg, protobuf, self.tlib_msgs)
 
