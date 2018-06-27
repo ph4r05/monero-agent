80cecfc7f6ef41cecf33f06a7d20a765f288b294
Dusan Klinec <dusan.klinec@gmail.com>
2018-06-27 22:50:47 +0200

    poc: devide reinit by default with user consent
    - we dont want users to wipe their trezors

--- monero_poc/trezor_init.py
+++ monero_poc/trezor_init.py
@@ -6,16 +6,19 @@
 import os
 from trezorlib import coins
 from trezorlib import tx_api
-from trezorlib.client import TrezorClientDebugLink
+from trezorlib.client import TrezorClientDebugLink, TrezorClient
 from trezorlib.transport import get_transport
 
 mnemonic12 = 'alcohol woman abuse must during monitor noble actual mixed trade anger aisle'
+debug_mode = False
 
 path = os.environ.get('TREZOR_PATH', 'udp:127.0.0.1:21324')
 wirelink = get_transport(path)
 debuglink = wirelink.find_debug()
-client = TrezorClientDebugLink(wirelink)
-client.set_debuglink(debuglink)
+client = TrezorClientDebugLink(wirelink) if debug_mode else TrezorClient(wirelink)
+if debug_mode:
+    client.set_debuglink(debuglink)
+
 client.transport.session_begin()
 
 client.wipe_device()
