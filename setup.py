b5a47dedb1a3e66ef4c0b5d2d1b94a42f2eb44b3
Dusan Klinec <dusan.klinec@gmail.com>
2018-07-20 22:25:11 +0200

    setup.py: develop mode added, builds messages

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
+import os
+import glob
+import subprocess
 import sys
 
-from setuptools import find_packages, setup
+from setuptools import setup, Command, find_packages
+from setuptools.command.build_py import build_py
+from setuptools.command.develop import develop
+from distutils.errors import DistutilsError
+
 
 version = "1.4.0"
 
@@ -51,6 +58,56 @@ docs_extras = [
 
 trezor_extras = ["trezor"]
 
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+TREZOR_COMMON = os.path.join(CWD, 'vendor', 'trezor-common')
+
+
+class PrebuildCommand(Command):
+    description = 'update vendored files (coins.json, protobuf messages)'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        # check for existence of the submodule directory
+        common_defs = os.path.join(TREZOR_COMMON, 'defs')
+        if not os.path.exists(common_defs):
+            raise DistutilsError('trezor-common submodule seems to be missing.\n' +
+                                 'Use "git submodule update --init" to retrieve it.')
+
+        # regenerate messages
+        try:
+            proto_srcs = glob.glob(os.path.join(TREZOR_COMMON, "protob", "*.proto"))
+            subprocess.check_call([
+                sys.executable,
+                os.path.join(TREZOR_COMMON, "protob", "pb2py"),
+                "-o", os.path.join(CWD, "monero_glue", "messages"),
+                "-P", "..protobuf",
+            ] + proto_srcs)
+        except Exception as e:
+            raise DistutilsError("Generating protobuf failed. Make sure you have 'protoc' in your PATH.") from e
+
+
+def _patch_prebuild(cls):
+    """Patch a setuptools command to depend on `prebuild`"""
+    orig_run = cls.run
+
+    def new_run(self):
+        self.run_command('prebuild')
+        orig_run(self)
+
+    cls.run = new_run
+
+
+_patch_prebuild(build_py)
+_patch_prebuild(develop)
+
+
 try:
     import pypandoc
 
@@ -91,5 +148,8 @@ setup(
         "docs": docs_extras,
         "tcry": tcry_extras,
         "trezor": trezor_extras,
+    },
+    cmdclass={
+        'prebuild': PrebuildCommand,
     },
 )
