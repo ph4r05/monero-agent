ceb94b62cfee6356b48fec5bf0c5268c5c0e9dea
Dusan Klinec <dusan.klinec@gmail.com>
2018-07-23 13:10:40 +0200

    pb sync

--- monero_glue/messages/MoneroWatchKey.py
+++ monero_glue/messages/MoneroWatchKey.py
@@ -4,6 +4,7 @@ from .. import protobuf as p
 
 
 class MoneroWatchKey(p.MessageType):
+    MESSAGE_WIRE_TYPE = 533
     FIELDS = {
         1: ('watch_key', p.BytesType, 0),
         2: ('address', p.BytesType, 0),
