83eced1cc1fae6cd645a358b9f3a5c4f5d139d43
Dusan Klinec <dusan.klinec@gmail.com>
2018-07-25 23:27:27 +0200

    xmr: misc.bip.bip39_deriv - BIP39 derivation from mnemonics added, trezor compatible

---
+++ monero_glue/misc/bip/bip39_deriv.py
@@ -0,0 +1,15 @@
+from Crypto.Protocol.KDF import PBKDF2
+import hmac
+import hashlib
+
+
+def mnemonics_to_seed(seed, passphrase=b''):
+    salt = b"mnemonic" + passphrase
+
+    def prf(p, s):
+        hx = hmac.new(p, msg=s, digestmod=hashlib.sha512)
+        return hx.digest()
+
+    res = PBKDF2(password=seed, salt=salt, dkLen=64, prf=prf, count=2048)
+    return res
+
