8a0a9d46f21fad85dbb89563f3cec8e398da5bc3
Dusan Klinec <dusan.klinec@gmail.com>
2018-07-26 01:05:39 +0200

    test agent_lite test - stronger transaction verification
    - test range proof
    - test mlsag correctness (verifies mlsag hash, tx prefix hash)
    - decrypt tx priv keys

--- monero_glue_test/test_agent_lite.py
+++ monero_glue_test/test_agent_lite.py
@@ -12,8 +12,12 @@ import aiounittest
 import pkg_resources
 from monero_glue.agent import agent_lite
 from monero_glue.hwtoken import token
-from monero_glue.xmr import crypto, monero, wallet, ring_ct
+from monero_glue.hwtoken.misc import StdObj
+from monero_glue.xmr import crypto, monero, wallet, ring_ct, common, mlsag2
+from monero_glue.hwtoken import misc
 from monero_serialize import xmrboost, xmrserialize, xmrtypes
+
+from monero_glue.xmr.enc import chacha_poly
 
 
 class AgentLiteTest(aiounittest.AsyncTestCase):
@@ -127,8 +131,8 @@ class AgentLiteTest(aiounittest.AsyncTestCase):
         """
         tagent = self.init_agent()
         txes = await tagent.sign_unsigned_tx(unsigned_tx)
-        await self.verify(txes[0], tagent.last_transaction_data())
-        return await self.receive(txes[0])
+        await self.verify(txes[0], tagent.last_transaction_data(), creds=self.get_creds())
+        return await self.receive(txes[0], [self.get_creds(), self.get_creds_01(), self.get_creds_02()])
 
     async def tx_sign_unsigned(self, unsigned_tx):
         """
@@ -171,13 +175,15 @@ class AgentLiteTest(aiounittest.AsyncTestCase):
 
         tagent = self.init_agent()
         txes = await tagent.sign_tx(pending.construction_data)
-        await self.verify(txes[0], tagent.last_transaction_data())
-        await self.receive(txes[0])
+        await self.verify(txes[0], tagent.last_transaction_data(), creds=self.get_creds())
+        await self.receive(txes[0], [self.get_creds(), self.get_creds_01(), self.get_creds_02()])
 
-    async def verify(self, tx, con_data=None):
+    async def verify(self, tx, con_data=None, creds=None):
         """
         Transaction verification
         :param tx:
+        :param con_data:
+        :param creds:
         :return:
         """
 
@@ -188,14 +194,16 @@ class AgentLiteTest(aiounittest.AsyncTestCase):
 
         await ar1.message(tx_obj, msg_type=xmrtypes.Transaction)
         extras = await monero.parse_extra_fields(tx_obj.extra)
+        monero.expand_transaction(tx_obj)
 
-        tx_pub = monero.find_tx_extra_field_by_type(
+        tx_pub = crypto.decodepoint(monero.find_tx_extra_field_by_type(
             extras, xmrtypes.TxExtraPubKey
-        ).pub_key
+        ).pub_key)
 
         additional_pub_keys = monero.find_tx_extra_field_by_type(
             extras, xmrtypes.TxExtraAdditionalPubKeys
         )
+        additional_pub_keys = [crypto.decodepoint(x) for x in additional_pub_keys.data] if additional_pub_keys is not None else None
 
         num_outs = len(tx_obj.vout)
 
@@ -206,16 +214,46 @@ class AgentLiteTest(aiounittest.AsyncTestCase):
             res = ring_ct.ver_range(C, rsig)
             self.assertTrue(res)
 
+        # Prefix hash
+        prefix_hash = await monero.get_transaction_prefix_hash(tx_obj)
+        is_simple = len(tx_obj.vin) > 1
 
+        self.assertEqual(prefix_hash, con_data.tx_prefix_hash)
+        tx_obj.rct_signatures.message = prefix_hash
 
+        # MLSAG hash
+        mlsag_hash = await monero.get_pre_mlsag_hash(tx_obj.rct_signatures)
 
-    async def receive(self, tx):
+        # Decrypt transaction key
+        tx_key = misc.compute_tx_key(creds.spend_key_private, prefix_hash, salt=con_data.enc_salt1, rand_mult=con_data.enc_salt2)[0]
+        key_buff = chacha_poly.decrypt_pack(tx_key, con_data.enc_keys)
+
+        tx_priv_keys = [crypto.decodeint(x) for x in common.chunk(key_buff, 32) if x]
+        tx_priv = tx_priv_keys[0]
+        tx_additional_priv = tx_priv_keys[1:]
+
+        # Verify mlsag signature
+        monero.recode_msg(tx_obj.rct_signatures.p.MGs, encode=False)
+        for idx in range(len(tx_obj.vin)):
+            if is_simple:
+                mix_ring = [x[1] for x in con_data.tx_data.sources[idx].outputs]
+                pseudo_out = crypto.decodepoint(bytes(tx_obj.rct_signatures.pseudoOuts[idx]))
+                self.assertTrue(mlsag2.ver_rct_mg_simple(
+                    mlsag_hash, tx_obj.rct_signatures.p.MGs[idx], mix_ring, pseudo_out
+                ))
+
+            else:
+                txn_fee_key = crypto.scalarmult_h(tx_obj.rct_signatures.txnFee)
+                mix_ring = [[x[1]] for x in con_data.tx_data.sources[idx].outputs]
+                self.assertTrue(mlsag2.ver_rct_mg(
+                    tx_obj.rct_signatures.p.MGs[idx], mix_ring, tx_obj.rct_signatures.outPk, txn_fee_key, mlsag_hash
+                ))
+
+    async def receive(self, tx, all_creds):
         """
         Test transaction receive with known view/spend keys of destinations.
         :return:
         """
-        wallet_creds = [self.get_creds(), self.get_creds_01(), self.get_creds_02()]
-
         # Unserialize the transaction
         tx_obj = xmrtypes.Transaction()
         reader = xmrserialize.MemoryReaderWriter(bytearray(tx))
@@ -236,7 +274,7 @@ class AgentLiteTest(aiounittest.AsyncTestCase):
         tx_money_got_in_outs = collections.defaultdict(lambda: 0)
         outs = []
 
-        for idx, creds in enumerate(wallet_creds):
+        for idx, creds in enumerate(all_creds):
             wallet_subs = {}
             for account in range(0, 5):
                 monero.compute_subaddresses(creds, account, range(5), wallet_subs)
