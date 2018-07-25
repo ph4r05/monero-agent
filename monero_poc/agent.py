a8173ad020cb8edaa26f0a31ccdd8b0e86cbd58e
Dusan Klinec <dusan.klinec@gmail.com>
2018-07-25 23:28:02 +0200

    poc: agent - mnemonic24 fix

--- monero_poc/agent.py
+++ monero_poc/agent.py
@@ -221,7 +221,7 @@ class HostAgent(cli.BaseCli):
 
     def do_init(self, line):
         mnemonic12 = "alcohol woman abuse must during monitor noble actual mixed trade anger aisle"
-        mnemonic24 = "permit universe parent weapon amused modify essay borrow tobacco budget walnut " "lunch consider gallery ride amazing frog forget treat market chapter velvet useless topple"
+        mnemonic24 = "permit universe parent weapon amused modify essay borrow tobacco budget walnut lunch consider gallery ride amazing frog forget treat market chapter velvet useless topple"
 
         mnemonic = mnemonic12 if len(line) == 0 or int(line) == 0 else mnemonic24
         self.trezor_proxy.client.wipe_device()
