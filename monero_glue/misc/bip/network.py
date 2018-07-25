d2d4f95535506cf476f621f3090243f38a12f5f2
Dusan Klinec <dusan.klinec@gmail.com>
2018-07-25 23:27:46 +0200

    xmr: misc.bip.network - default BIP44 derivation fix, Trezor, Ledger compatible

--- monero_glue/misc/bip/network.py
+++ monero_glue/misc/bip/network.py
@@ -202,4 +202,4 @@ class MoneroMainNet(object):
     SECRET_KEY = 0x80  # int(0x80) = 128  # Used for WIF format
     EXT_PUBLIC_KEY = 0x0488B21E  # Used to serialize public BIP32 addresses
     EXT_SECRET_KEY = 0x0488ADE4  # Used to serialize private BIP32 addresses
-    BIP32_PATH = "m/44'/128'/0'/"
+    BIP32_PATH = "m/44'/128'/0'/0/0"
