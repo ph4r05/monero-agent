13a516e8fea9daf53549971ad2eefe579fc84eec
Dusan Klinec <dusan.klinec@gmail.com>
2018-07-26 01:07:27 +0200

    test: test only glue tests
    - avoid further trezor tests on travis

--- test-travis.sh
+++ test-travis.sh
@@ -2,8 +2,10 @@
 pip install .[dev]
 
 # trezor-crypto backend
-EC_BACKEND_FORCE=1 EC_BACKEND=1 python -m unittest discover $*
+# EC_BACKEND_FORCE=1 EC_BACKEND=1 python -m unittest discover $*
+EC_BACKEND_FORCE=1 EC_BACKEND=1 python -m unittest monero_glue_test/test_*.py
 
 
 # python backend
-EC_BACKEND=0 python -m unittest discover $*
+# EC_BACKEND=0 python -m unittest discover $*
+EC_BACKEND=0 python -m unittest monero_glue_test/test_*.py
