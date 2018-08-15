eb16e046ef4efc1013444994ce9741b9ea891a51
Dusan Klinec <dusan.klinec@gmail.com>
2018-08-15 17:51:12 +0200

    bp: consts, iter proto on KeyV, computed KeyV, BP builder
    - aL, aR computed from value on demand (saves RAM)
    - aL, aR are dynamically evaluated vectors - same API as normal vectors, usable with same functions, read only though
    - sL, sR random masks deterministically generated evaluated vectors, may be used if memory problems occur.
    - sL, sR random mask vector generation

--- monero_glue/xmr/bulletproof.py
+++ monero_glue/xmr/bulletproof.py
@@ -5,6 +5,8 @@
 
 import logging
 
+from monero_serialize.core.int_serialize import dump_uvarint_b
+
 from monero_glue.xmr import common, crypto, mlsag2, monero
 from monero_serialize import xmrtypes
 
@@ -14,6 +16,12 @@ ATOMS = 64
 
 # curve size
 l = 2**252 + 3*610042537739*15158679415041928064055629
+
+
+# Constants
+
+ZERO = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+ONE = b'\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
 
 
 #
@@ -45,6 +53,11 @@ def _ensure_dst_key(dst=None):
     if dst is None:
         dst = bytearray(32)
     return dst
+
+
+def copy_key(dst, src):
+    for i in range(32):
+        dst[i] = src[i]
 
 
 def invert(dst, x):
@@ -129,9 +142,6 @@ def sk_gen(dst=None):
     pass  # TODO:
 
 
-
-
-
 #
 #
 #
@@ -143,6 +153,7 @@ class KeyV(object):
     Constant precomputed buffers = bytes, frozen. Same operation as normal.
     """
     def __init__(self, elems=64, src=None, buffer=None):
+        self.current_idx = 0
         self.d = None
         self.size = elems
         if src:
@@ -173,6 +184,17 @@ class KeyV(object):
         for i in range(32):
             ck[i] = value[i]
 
+    def __iter__(self):
+        self.current_idx = 0
+        return self
+
+    def __next__(self):
+        if self.current_idx > self.size:
+            raise StopIteration
+        else:
+            self.current_idx += 1
+            return self[self.current_idx - 1]
+
     def slice(self, res, start, stop):
         for i in range(start, stop):
             res[i - start] = self[i]
@@ -197,6 +219,33 @@ class KeyV(object):
         else:
             self.d = bytearray(nsize * 32)
         self.size = nsize
+
+
+class KeyVEval(KeyV):
+    """
+    KeyVector computed / evaluated on demand
+    """
+    def __init__(self, elems=64, src=None):
+        self.size = elems
+        self.fnc = src
+
+    def __getitem__(self, item):
+        return memoryview(self.fnc(item))
+
+    def __setitem__(self, key, value):
+        raise ValueError('Constant vector')
+
+    def slice(self, res, start, stop):
+        raise ValueError('Not supported')
+
+    def slice_r(self, start, stop):
+        raise ValueError('Not supported')
+
+    def copy(self, dst=None):
+        raise ValueError('Not supported')
+
+    def resize(self, nsize):
+        raise ValueError('Not supported')
 
 
 def _ensure_dst_keyvect(dst=None, size=None):
@@ -265,4 +314,61 @@ def vector_scalar2(a, x, dst=None):
 def hash_cache_mash(hash_cache, mash0, mash1, mash2=None, mash3=None):
     pass  # TODO:
 
+
+class BulletProofBuilder(object):
+    def __init__(self, value, mask):
+        self.value = value
+        self.value_enc = crypto.encodeint(value)
+        self.gamma = mask
+        self.gamma_enc = crypto.encodeint(mask)
+        self.proof_sec = crypto.random_bytes(128)
+
+    def aL(self, i, dst=None):
+        dst = _ensure_dst_key(dst)
+        if self.value_enc[i//8] & (1 << i%8):
+            copy_key(dst, ONE)
+        else:
+            copy_key(dst, ZERO)
+        return dst
+
+    def aR(self, i, dst=None):
+        dst = _ensure_dst_key(dst)
+        a_tmp = self.aL(i)
+        sc_sub(dst, a_tmp, ONE)
+        return dst
+
+    def aL_vct(self):
+        return KeyVEval(64, lambda x: self.aL(x))
+
+    def aR_vct(self):
+        return KeyVEval(64, lambda x: self.aR(x))
+
+    def _det_mask(self, i, is_sL=True, dst=None):
+        dst = _ensure_dst_key(dst)
+        src = crypto.keccak_2hash(self.proof_sec + (b"sL" if is_sL else b"sR") + dump_uvarint_b(i))
+        sc = crypto.new_scalar()
+        crypto.decodeint_into(sc, src)
+        crypto.encodeint_into(sc, dst)
+        return dst
+
+    def sL(self, i, dst=None):
+        return self._det_mask(i, True, dst)
+
+    def sR(self, i, dst=None):
+        return self._det_mask(i, False, dst)
+
+    def sL_vct(self):
+        return KeyVEval(64, lambda x: self.sL(x))
+
+    def sR_vct(self):
+        return KeyVEval(64, lambda x: self.sR(x))
+
+    def sX_gen(self):
+        buff = bytearray(64 * 32)
+        buff_mv = memoryview(buff)
+        sc = crypto.new_scalar()
+        for i in range(64):
+            crypto.random_scalar_into(sc)
+            crypto.encodeint_into(sc, buff_mv[i * 32 : (i + 1) * 32])
+        return KeyV(buffer=buff)
 
