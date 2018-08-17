e016eac338ef4e88d8d3e706f15fb20ba59e95cb
Dusan Klinec <dusan.klinec@gmail.com>
2018-08-17 02:22:59 +0200

    bp: custom gc function

--- monero_glue/xmr/bulletproof.py
+++ monero_glue/xmr/bulletproof.py
@@ -517,6 +517,11 @@ class BulletProofBuilder(object):
         self.v_sL = None
         self.v_sR = None
         self.tmp_sc_1 = crypto.new_scalar()
+        self.gc_fnc = gc.collect
+
+    def gc(self):
+        if self.gc_fnc:
+            self.gc_fnc()
 
     def set_input(self, value=None, mask=None):
         self.value = value
@@ -602,7 +607,7 @@ class BulletProofBuilder(object):
         hash_cache_mash(y, hash_cache, A, S)
         hash_to_scalar(hash_cache, y)
         copy_key(z, hash_cache)
-        gc.collect()
+        self.gc()
 
         # Polynomial construction before PAPER LINE 46
         t0 = _ensure_dst_key()
@@ -610,7 +615,7 @@ class BulletProofBuilder(object):
         t2 = _ensure_dst_key()
 
         yN = vector_powers(y, BP_N)
-        gc.collect()
+        self.gc()
 
         ip1y = inner_product(self.oneN, yN)
         sc_muladd(t0, z, ip1y, t0)
@@ -627,20 +632,20 @@ class BulletProofBuilder(object):
         sc_mul(zcu, zsq, z)
         sc_mulsub(k, zcu, self.ip12, k)
         sc_add(t0, t0, k)
-        gc.collect()
+        self.gc()
 
         # step 2
         vpIz = vector_scalar(self.oneN, z)
         aL_vpIz = vector_subtract(self.v_aL, vpIz)
         aR_vpIz = vector_add(self.v_aR, vpIz)
         del vpIz
-        gc.collect()
+        self.gc()
 
         HyNsR = hadamard(yN, self.v_sR)
         ip1 = inner_product(aL_vpIz, HyNsR)
         ip3 = inner_product(self.v_sL, HyNsR)
         del HyNsR
-        gc.collect()
+        self.gc()
 
         sc_add(t1, t1, ip1)
 
@@ -653,7 +658,7 @@ class BulletProofBuilder(object):
         vector_add(tmp_vct, vp2zsq, tmp_vct)
         ip2 = inner_product(self.v_sL, tmp_vct)
 
-        gc.collect()
+        self.gc()
         sc_add(t1, t1, ip2)
         sc_add(t2, t2, ip3)
 
@@ -676,30 +681,30 @@ class BulletProofBuilder(object):
         sc_muladd(taux, tau2, xsq, taux)
         sc_muladd(taux, self.gamma_enc, zsq, taux)
         sc_muladd(mu, x, rho, alpha)
-        gc.collect()
+        self.gc()
 
         # PAPER LINES 54-57
         vector_add(aL_vpIz, vector_scalar(self.v_sL, x), l)
         self.v_sL = None
         del aL_vpIz
-        gc.collect()
+        self.gc()
 
         # Originally:
         # vector_add(hadamard(yN, vector_add(aR_vpIz, vector_scalar(self.v_sR, x))), vp2zsq, r)
         vector_scalar(self.v_sR, x, tmp_vct)
         vector_add(aR_vpIz, tmp_vct, tmp_vct)
         del aR_vpIz
-        gc.collect()
+        self.gc()
 
         hadamard(yN, tmp_vct, tmp_vct)
         del yN
-        gc.collect()
+        self.gc()
 
         vector_add(tmp_vct, vp2zsq, r)
         self.v_sR = None
         del vp2zsq
         del tmp_vct
-        gc.collect()
+        self.gc()
 
         inner_product(l, r, t)
         hash_cache_mash(x_ip, hash_cache, x, taux, mu, t)
@@ -718,7 +723,7 @@ class BulletProofBuilder(object):
             Gprime[i] = self.Gprec[i]
             scalarmult_key(Hprime[i], self.Hprec[i], yinvpow)
             sc_mul(yinvpow, yinvpow, yinv)
-        gc.collect()
+        self.gc()
 
         round = 0
         nprime = BP_N
@@ -741,7 +746,7 @@ class BulletProofBuilder(object):
             _tmp_vct_2.resize(nprime, chop=True)
             _tmp_vct_3.resize(nprime, chop=True)
             _tmp_vct_4.resize(nprime, chop=True)
-            gc.collect()
+            self.gc()
 
             # PAPER LINES 16-17
             cL = inner_product(
@@ -773,7 +778,7 @@ class BulletProofBuilder(object):
                 bprime.slice(_tmp_vct_4, 0, nprime),
                 R[round],
             )
-            gc.collect()
+            self.gc()
 
             sc_mul(tmp, cR, x_ip)
             add_keys(R[round], R[round], scalarmult_key(_tmp_k_1, XMR_H, tmp))
@@ -800,7 +805,7 @@ class BulletProofBuilder(object):
                 ),
                 Hprime,
             )
-            gc.collect()
+            self.gc()
 
             # PAPER LINES 28-29
             vector_add(
@@ -822,7 +827,7 @@ class BulletProofBuilder(object):
             )
 
             round += 1
-            gc.collect()
+            self.gc()
 
         copy_key(aprime0, aprime[0])
         copy_key(bprime0, bprime[0])
@@ -855,13 +860,13 @@ class BulletProofBuilder(object):
         r = _ensure_dst_keyvect(None, BP_N)
 
         self.init_vct()
-        gc.collect()
+        self.gc()
 
         self.prove_s1(V, A, S, T1, T2, taux, mu, t, x_ip, y, hash_cache, l, r)
-        gc.collect()
+        self.gc()
 
         self.prove_s2(x_ip, y, hash_cache, l, r, L, R, aprime0, bprime0)
-        gc.collect()
+        self.gc()
 
         return Bulletproof(
             V=[V],
@@ -941,7 +946,7 @@ class BulletProofBuilder(object):
         sc_mul(xsq, x, x)
         scalarmult_key(tmp, proof.T2, xsq)
         add_keys(L61Right, L61Right, tmp)
-        gc.collect()
+        self.gc()
 
         if L61Right != L61Left:
             raise ValueError("Verification failure 1")
@@ -1007,6 +1012,7 @@ class BulletProofBuilder(object):
 
         del g_scalar
         del h_scalar
+        self.gc()
 
         # PAPER LINE 26
         pprime = _ensure_dst_key()
@@ -1027,7 +1033,7 @@ class BulletProofBuilder(object):
         sc_mul(tmp, tmp, x_ip)
         scalarmult_key(tmp, XMR_H, tmp)
         add_keys(tmp, tmp, inner_prod)
-        gc.collect()
+        self.gc()
 
         if pprime != tmp:
             raise ValueError("Verification failure step 2")
