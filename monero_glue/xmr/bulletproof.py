722decd0c9eb8d21a93d00545230a15abdea5e16
Dusan Klinec <dusan.klinec@gmail.com>
2018-08-17 13:27:27 +0200

    bp: const KeyV

--- monero_glue/xmr/bulletproof.py
+++ monero_glue/xmr/bulletproof.py
@@ -230,11 +230,12 @@ class KeyV(object):
     Constant precomputed buffers = bytes, frozen. Same operation as normal.
     """
 
-    def __init__(self, elems=64, src=None, buffer=None):
+    def __init__(self, elems=64, src=None, buffer=None, const=False):
         self.current_idx = 0
         self.d = None
         self.mv = None
         self.size = elems
+        self.const = const
         if src:
             self.d = bytearray(src.d)
             self.size = src.size
@@ -263,6 +264,8 @@ class KeyV(object):
         :param value:
         :return:
         """
+        if self.const:
+            raise ValueError('Constant KeyV')
         ck = self[key]
         for i in range(32):
             ck[i] = value[i]
@@ -504,10 +507,10 @@ class BulletProofBuilder(object):
         self.gamma = None
         self.gamma_enc = None
         self.proof_sec = None
-        self.Gprec = KeyV(buffer=BP_GI_PRE)
-        self.Hprec = KeyV(buffer=BP_HI_PRE)
-        self.oneN = KeyV(buffer=BP_ONE_N)
-        self.twoN = KeyV(buffer=BP_TWO_N)
+        self.Gprec = KeyV(buffer=BP_GI_PRE, const=True)
+        self.Hprec = KeyV(buffer=BP_HI_PRE, const=True)
+        self.oneN = KeyV(buffer=BP_ONE_N, const=True)
+        self.twoN = KeyV(buffer=BP_TWO_N, const=True)
         self.ip12 = BP_IP12
         self.v_aL = None
         self.v_aR = None
