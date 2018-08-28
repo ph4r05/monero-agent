2f6c7284b84605bc888ce4363137896c88fae806
Dusan Klinec <dusan.klinec@gmail.com>
2018-08-28 23:01:57 +0200

    protocol: ki sync with buffer

--- monero_glue/protocol/key_image_sync.py
+++ monero_glue/protocol/key_image_sync.py
@@ -63,7 +63,11 @@ class KeyImageSync(object):
             raise ValueError("Blocked")
         if len(tds.tdis) == 0:
             raise ValueError("Empty")
+
         resp = []
+        buff = bytearray(32 * 3)
+        buff_mv = memoryview(buff)
+
         for td in tds.tdis:
             self.c_idx += 1
             if self.c_idx >= self.num:
@@ -76,9 +80,9 @@ class KeyImageSync(object):
                 self.creds, self.subaddresses, td
             )
 
-            buff = crypto.encodepoint(ki)
-            buff += crypto.encodeint(sig[0][0])
-            buff += crypto.encodeint(sig[0][1])
+            crypto.encodepoint_into(ki, buff_mv[0:32])
+            crypto.encodeint_into(sig[0][0], buff_mv[32:64])
+            crypto.encodeint_into(sig[0][1], buff_mv[64:])
 
             nonce, ciph, tag = chacha_poly.encrypt(self.enc_key, buff)
             eki = MoneroExportedKeyImage(iv=nonce, tag=tag, blob=ciph)
