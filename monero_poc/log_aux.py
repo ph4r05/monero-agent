4fc0d48424c1d9c238c215743e58074907951d9f
Dusan Klinec <dusan.klinec@gmail.com>
2018-09-20 18:46:34 +0200

    poc: logger extended with time normalization

--- monero_poc/log_aux.py
+++ monero_poc/log_aux.py
@@ -21,8 +21,10 @@ def tic2sec(tic):
 
 class LogAnalyzer(object):
     def __init__(self):
+        self.args = None
         self.max_line_len = 0
         self.time_prev = 0
+        self.time_prev_o = 0
         self.time_ref = 0
         self.time_ref_r = 0
         self.mem_alloc_prev = 0
@@ -30,6 +32,10 @@ class LogAnalyzer(object):
         self.mem_alloc_max = 0
         self.serial_device = None
         self.tee_file = None
+        self.initial_ticks = None
+
+    def print_line(self, line):
+        print(line)
 
     def process(self, line):
         line = line.strip()
@@ -39,14 +45,30 @@ class LogAnalyzer(object):
 
         m = re.match(r'^(\d+)\s([^\s]+?)\s([^\s]+?)\s(.*)$', line)
         if m is None:
-            print(line)
+            self.print_line(line)
             return
 
         ctime_r = time.time()
         ctime = int(m.group(1))
+        ctime_o = ctime
         c_prev = self.time_prev
         c_prev_alloc = self.mem_alloc_prev
+
+        if self.args.no_time:
+            line = re.sub(r'^\d+\s*', '', line)
+
+        elif self.args.norm_time:
+            if self.initial_ticks is None:
+                self.initial_ticks = ctime
+                ctime = 0
+            elif ctime < self.time_prev_o:
+                ctime = self.time_prev + (ctime + (4294967296 - self.time_prev_o))
+            else:
+                ctime = self.time_prev + (ctime - self.time_prev_o)
+            line = re.sub(r'^\d+', '%08d' % ctime, line)
+
         self.time_prev = ctime
+        self.time_prev_o = ctime_o
 
         if '----diagnostic' in line:
             self.time_ref = ctime
@@ -79,15 +101,19 @@ class LogAnalyzer(object):
 
             memstr = 'Alloc diff: %5d, refdi: %5d' % (mem_alloc - self.mem_alloc_ref, mem_alloc - c_prev_alloc)
 
+        if self.args.no_aug:
+            self.print_line(line)
+            return
+
         ldiff = self.max_line_len - len(line)
-        print('%s%s |  AbsTime: %7.3f,   Diff %5.3f  | %s' % (line, ' '*ldiff, abs_time, diff_time, memstr))
+        self.print_line('%s%s |  AbsTime: %7.3f,   Diff %5.3f  | %s' % (line, ' '*ldiff, abs_time, diff_time, memstr))
 
         if '====' in line or '####' in line:
             abs_r = ''
             if self.serial_device:
                 abs_time_r = ctime_r - self.time_ref_r
                 abs_r = 'r: %7.2f, ticks p.s.: %7.3f' % (abs_time_r, (ctime - self.time_ref) / float(abs_time_r))
-            print(' ++ TOTAL: %7.2f, %s mem max: %s' % (abs_time, abs_r, self.mem_alloc_max - self.mem_alloc_ref))
+            self.print_line(' ++ TOTAL: %7.2f, %s mem max: %s' % (abs_time, abs_r, self.mem_alloc_max - self.mem_alloc_ref))
 
             self.time_ref = ctime
             self.time_ref_r = ctime_r
@@ -125,13 +151,25 @@ class LogAnalyzer(object):
 
     def main(self):
         parser = argparse.ArgumentParser(description='Trezor log reader and parser')
-        parser.add_argument('--serial', default=None, help='Serial device to read from')
-        parser.add_argument('--brate', type=int, default=115200, help='Baud rate')
-        parser.add_argument("--retry", dest="retry", default=True, action="store_const", const=True, help="Retry reconnect")
-        parser.add_argument("--tee", dest="tee", default=None, help="File to copy raw output to")
-        parser.add_argument('files', metavar='FILE', nargs='*', help='files to read, if empty, stdin is used')
+        parser.add_argument('--serial', default=None,
+                            help='Serial device to read from')
+        parser.add_argument('--brate', type=int, default=115200,
+                            help='Baud rate')
+        parser.add_argument("--retry", dest="retry", default=True, action="store_const", const=True,
+                            help="Retry reconnect")
+        parser.add_argument("--norm-time", dest="norm_time", default=True, action="store_const", const=True,
+                            help="Normalize time")
+        parser.add_argument("--no-time", dest="no_time", default=True, action="store_const", const=True,
+                            help="Do not show time")
+        parser.add_argument("--no-aug", dest="no_aug", default=True, action="store_const", const=True,
+                            help="Do not augment the log output")
+        parser.add_argument("--tee", dest="tee", default=None,
+                            help="File to copy raw output to")
+        parser.add_argument('files', metavar='FILE', nargs='*',
+                            help='files to read, if empty, stdin is used')
         args = parser.parse_args()
 
+        self.args = args
         if args.tee:
             if os.path.exists(args.tee):
                 raise ValueError('Tee file already exists')
