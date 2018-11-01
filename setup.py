3d48912c5509d7e2eba90fd2cdc2c8dbf1f487ac
Dusan Klinec <dusan.klinec@gmail.com>
2018-11-01 10:28:17 +0100

    v1.7.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from distutils.errors import DistutilsError
 logger = logging.getLogger(__name__)
 
 
-version = "1.7.1"
+version = "1.7.2"
 
 # Please update tox.ini when modifying dependency version requirements
 install_requires = [
