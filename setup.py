e23ad7a4970b29ef6ad13cd47d058725b50e480a
Dusan Klinec <dusan.klinec@gmail.com>
2018-11-27 22:54:42 +0100

    v1.7.6

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from distutils.errors import DistutilsError
 logger = logging.getLogger(__name__)
 
 
-version = "1.7.5"
+version = "1.7.6"
 
 # Please update tox.ini when modifying dependency version requirements
 install_requires = [
